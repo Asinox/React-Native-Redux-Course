fe24186e5421595aee8d119cc464ef4ce2702cfc
Juan Miguel Calcaño <jmiguel@diariolibre.com>
2018-12-19 16:25:20 -0400

    First Commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class AlbumList extends Component {
+
+    componentWillMount() {
+        console.log('componentWillMount in AlbumList');
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Album List!!!!!</Text>
+            </View>
+        );
+    }
+    
+}
