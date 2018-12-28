748fec2ee47e96f413e456c13e78264a961824dc
Juan Miguel Calcano <asinox@gmail.com>
2018-12-27 22:11:04 -0400

    Album Detail

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from "./AlbumDetail";
 
-export default class AlbumList extends Component {
+class AlbumList extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = { albums:[] };
+    }
+
 
     componentWillMount() {
-        axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => this.setState({albums: response.data}));
+    }
+
+    renderAlbums(){
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album}/>
+        );
     }
 
     render() {
+
+        console.log(this.state);
+
         return (
             <View>
-                <Text>Album List!!!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
     
 }
+
+export default AlbumList;
